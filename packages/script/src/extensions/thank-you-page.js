3b5341679eef8e6199b30aecedc09ed5f7a77d08
alexandre-sarfati <alexandre@docduo.com>
2026-03-08 02:02:41 +0100

    impr: support for pimms_redirected

--- packages/script/src/extensions/thank-you-page.js
+++ packages/script/src/extensions/thank-you-page.js
@@ -4,20 +4,20 @@ const initThankYouPage = () => {
   const thankYouUrl = DOMAINS_CONFIG['thank-you'];
   if (!thankYouUrl || typeof thankYouUrl !== 'string') return;
 
-  if (!location.pathname.startsWith('/thanks/')) return;
+  const path = location.pathname;
+  if (!path.startsWith('/thanks/') && path !== '/members/thanks') return;
 
-  var key = 'pimms_thanks_fired_' + location.pathname;
+  if (new URLSearchParams(location.search).has('pimms_redirected')) return;
+
+  var key = 'pimms_thanks_fired_' + path;
   try {
     if (sessionStorage.getItem(key)) return;
     sessionStorage.setItem(key, '1');
   } catch (e) {}
 
-  fetch(thankYouUrl, {
-    method: 'GET',
-    mode: 'no-cors',
-    credentials: 'include',
-    keepalive: true,
-  });
+  var tyUrl = new URL(thankYouUrl);
+  tyUrl.searchParams.set('pimms_redirect', location.href);
+  window.location.href = tyUrl.toString();
 };
 
 if (window._pimmsAnalytics) {
